6cd5a9bdd6165c28bd72236f4dc7d03dfda14664
ibarbylev <ibarbylev@gmail.com>
2022-06-17 22:07:03 +0300

    add first test

--- storage/tests.py
+++ storage/tests.py
@@ -1,6 +1,28 @@
 from django.test import TestCase
-# from rest_framework.test import APIRequestFactory
-#
-# # Using the standard RequestFactory API to create a form POST request
-# factory = APIRequestFactory()
-# request = factory.post('api/is_exists/', {'title': 'new idea'})
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from storage.models import Client
+
+
+class ClientTests(APITestCase):
+    def test_get_data(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('api:is_exist')
+        data = {}
+        response = self.client.get(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Client.objects.count(), 0)
+
+    # def test_create_new_client(self):
+    #     """
+    #     Ensure we can create a new account object.
+    #     """
+    #     url = reverse('is_exist')
+    #     data = {'name': 'DabApps'}
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Client.objects.count(), 1)
+    #     self.assertEqual(Client.objects.get().name, 'DabApps')
--- storage/urls.py
+++ storage/urls.py
@@ -4,7 +4,7 @@ from storage import views
 app_name = 'api'
 urlpatterns = [
     # path('api/is_exist/', views.ClientList.as_view()),
-    path('is_exist/', views.ClientIsExists.as_view()),  # method POST
+    path('is_exist/', views.ClientIsExists.as_view(), name='is_exist'),  # method POST
     path('delete-sleeps/<int:pk>/', views.ClientDeleteClientSleeps.as_view()),  # method PUT
     path('add-sleeps/<int:pk>/', views.ClientAddSleeps.as_view()),  # method PUT
     path('consultants/', views.ConsultantList.as_view()),  # method GET
